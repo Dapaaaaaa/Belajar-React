14e8fd108729398d5c0148bbaafcee3482f7a750
Dapaaaaaa <daffaharahap191205@gmail.com>
2026-02-17 14:33:38 +0800

    First Commit: Setup React & Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+  )
+}
+
+export default App
