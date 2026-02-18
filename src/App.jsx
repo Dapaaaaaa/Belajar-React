1901e193ea867814cbcda149fa42ad46389d3845
DaPC <daffaharahap191205@gmail.com>
2026-02-18 22:32:48 +0800

    feat: React Const

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,16 @@ function ButtonBlack() {
     return (
         <div>
                 <button className="h-10 px-6 font-semibold rounded-md bg-black text-white cursor-pointer" type="submit"> Buy Now</button>
-            </div>
+        </div>
+    );
+}
+
+// Constanta
+const ButtonRed = () => {
+    return (
+        <div>
+                <button className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white cursor-pointer" type="submit"> Buy Now</button>
+        </div>
     );
 }
 
@@ -24,8 +33,11 @@ function App() {
 
   return (
       <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-          <Button></Button>
-          <ButtonBlack></ButtonBlack>
+          <div className="flex gap-x-3">
+            <Button></Button>
+            <ButtonBlack></ButtonBlack>
+            <ButtonRed></ButtonRed>
+          </div>
       </div>
   )
 }
