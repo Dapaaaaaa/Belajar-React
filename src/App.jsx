42c95b1601811fbc7a7f0c65d63148661d74de22
DaPC <daffaharahap191205@gmail.com>
2026-02-18 22:25:14 +0800

    feat: React Function

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,27 @@
         render() {
             return (
             <div>
-                <button className="h-10 px-6 font-semibold rounded-md bg-black text-white cursor-pointer" type="submit"> Buy Now</button>
+                <button className="h-10 px-6 font-semibold rounded-md bg-white text-black cursor-pointer" type="submit"> Buy Now</button>
             </div>
             );
         }
     }
 
+// Function
+function ButtonBlack() {
+    return (
+        <div>
+                <button className="h-10 px-6 font-semibold rounded-md bg-black text-white cursor-pointer" type="submit"> Buy Now</button>
+            </div>
+    );
+}
+
 function App() {
 
   return (
       <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-          <Button>Klik disini</Button>
+          <Button></Button>
+          <ButtonBlack></ButtonBlack>
       </div>
   )
 }
