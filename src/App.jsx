6c5faf90cd12411e6bad94805cef9651196d48cc
DaPC <daffaharahap191205@gmail.com>
2026-02-18 23:01:44 +0800

    feat: React (Youtube)

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,40 @@ import React from "react";
 // }
 
 // Constanta (Versi Update)
-const Button = ({variant = "slate", children}) => {
-    const variants = {
-        red: "bg-red-700 hover:bg-red-800",
-        slate: "bg-slate-700 hover:bg-slate-800",
-        blue: "bg-blue-700 hover:bg-blue-800",
-    };
+// const Button = ({variant = "slate", children}) => {
+//     const variants = {
+//         red: "bg-red-700 hover:bg-red-800",
+//         slate: "bg-slate-700 hover:bg-slate-800",
+//         blue: "bg-blue-700 hover:bg-blue-800",
+//     };
+
+//     return (
+//         <div>
+//         <button
+//             className={`h-10 px-6 font-semibold rounded-md transition ${variants[variant]} text-white cursor-pointer`}
+//             type="submit"
+//         >
+//             {" "}
+//             Buy Now
+//             {/* Children */}
+//         </button>
+//         </div>
+//     );
+// };
+
+// Versi Youtube
+const Button = (props) => {
+    // Nilai Default 
+    const {variant = 'bg-black', children = 'Default'} = props;
 
     return (
         <div>
         <button
-            className={`h-10 px-6 font-semibold rounded-md transition ${variants[variant]} text-white cursor-pointer`}
+            className={`h-10 px-6 font-semibold rounded-md transition ${variant} text-white cursor-pointer`}
             type="submit"
         >
             {" "}
-            Buy Now
+            {children}
         </button>
         </div>
     );
@@ -45,9 +64,9 @@ function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
       <div className="flex gap-x-3">
-        <Button variant="red"></Button>
-        <Button variant="blue"></Button>
-        <Button variant="slate"></Button>
+        <Button variant="bg-red-700 hover:bg-red-800">Merah</Button>
+        <Button variant="bg-blue-700 hover:bg-blue-800">Biru</Button>
+        <Button></Button>
       </div>
     </div>
   );
