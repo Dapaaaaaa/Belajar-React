b90699006be8039dff2893d0de65734f81942234
DaPC <daffaharahap191205@gmail.com>
2026-02-18 22:47:21 +0800

    feat: React Const (New Version)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,56 @@
- import React from "react";
- 
+import React from "react";
+
 //  Komponen
- class Button extends React.Component{
-        render() {
-            return (
-            <div>
-                <button className="h-10 px-6 font-semibold rounded-md bg-white text-black cursor-pointer" type="submit"> Buy Now</button>
-            </div>
-            );
-        }
-    }
+//  class Button extends React.Component{
+//         render() {
+//             return (
+//             <div>
+//                 <button className="h-10 px-6 font-semibold rounded-md bg-white text-black cursor-pointer" type="submit"> Buy Now</button>
+//             </div>
+//             );
+//         }
+//     }
 
 // Function
-function ButtonBlack() {
-    return (
-        <div>
-                <button className="h-10 px-6 font-semibold rounded-md bg-black text-white cursor-pointer" type="submit"> Buy Now</button>
-        </div>
-    );
-}
+// function ButtonBlack() {
+//     return (
+//         <div>
+//                 <button className="h-10 px-6 font-semibold rounded-md bg-black text-white cursor-pointer" type="submit"> Buy Now</button>
+//         </div>
+//     );
+// }
 
-// Constanta
-const ButtonRed = () => {
+// Constanta (Versi Update)
+const Button = ({variant = "slate", children}) => {
+    const variants = {
+        red: "bg-red-700 hover:bg-red-800",
+        slate: "bg-slate-700 hover:bg-slate-800",
+        blue: "bg-blue-700 hover:bg-blue-800",
+    };
+
     return (
         <div>
-                <button className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white cursor-pointer" type="submit"> Buy Now</button>
+        <button
+            className={`h-10 px-6 font-semibold rounded-md transition ${variants[variant]} text-white cursor-pointer`}
+            type="submit"
+        >
+            {" "}
+            Buy Now
+        </button>
         </div>
     );
-}
+};
 
 function App() {
-
   return (
-      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-          <div className="flex gap-x-3">
-            <Button></Button>
-            <ButtonBlack></ButtonBlack>
-            <ButtonRed></ButtonRed>
-          </div>
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="red"></Button>
+        <Button variant="blue"></Button>
+        <Button variant="slate"></Button>
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
