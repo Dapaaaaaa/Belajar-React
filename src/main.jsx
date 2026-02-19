be61fa888d9b7042622130221db9fd3eed6747d3
DaPC <daffaharahap191205@gmail.com>
2026-02-19 21:50:30 +0800

    perf: Delete App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage from './pages/login.jsx'
+import RegisterPage from './pages/register.jsx'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter([ 
+  {
+    path: "/",
+    element: <div>Hello Guys</div>
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
