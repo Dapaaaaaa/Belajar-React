640e2590871f45edddd89d32404a8752fc5b1afb
DaPC <daffaharahap191205@gmail.com>
2026-04-09 22:51:42 +0800

    refactor(product.service): Change format from jsx to js

--- src/components/fragments/FormLogin.jsx
+++ src/components/fragments/FormLogin.jsx
@@ -5,9 +5,9 @@ import { useEffect, useRef } from "react";
 const FormLogin = () => {
   const handleLogin = (e) => {
     e.preventDefault();
-    localStorage.setItem("email", e.target.email.value);
-    localStorage.setItem("password", e.target.password.value);
-    window.location.href = "/products";
+    // localStorage.setItem("email", e.target.email.value);
+    // localStorage.setItem("password", e.target.password.value);
+    // window.location.href = "/products";
   };
 
   const emailRef = useRef(null);
