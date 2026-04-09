0709234efbb5d04b784d7afb98a8832b23676543
DaPC <daffaharahap191205@gmail.com>
2026-04-09 21:13:08 +0800

    feat: using useRef for automatic focus on email when login

--- src/components/elements/input/index.jsx
+++ src/components/elements/input/index.jsx
@@ -1,16 +1,16 @@
 import Label from "./label";
 import Input from "./input";
+import { forwardRef } from "react";
 
-const InputForm = (props) => {
+const InputForm = forwardRef((props, ref) => {
   const { label, name, type, placeholder } = props;
 
   return (
     <div className="mb-6">
       <Label htmlFor={name}>{label}</Label>
       {/* Disebut self closing tag ( < /> ) */}
-      <Input name={name} type={type} placeholder={placeholder} />
+      <Input name={name} type={type} placeholder={placeholder} ref={ref}/>
     </div>
   );
-};
-
+});
 export default InputForm;
